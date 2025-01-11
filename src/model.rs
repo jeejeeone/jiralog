dc19da0e040ca6fae23d3dbb938b84a418ebfd60
jeejeeone <jarijoki1@gmail.com>
2025-01-11 22:14:16 +0200

    reformat

--- src/model.rs
+++ src/model.rs
@@ -11,7 +11,7 @@ pub struct WorklogRecord {
     pub description: String,
     pub started_date: DateTime<FixedOffset>,
     pub committed: bool,
-    pub id: String
+    pub id: String,
 }
 
 pub struct Configuration {
@@ -19,7 +19,7 @@ pub struct Configuration {
     pub jira_cloud_instance: Option<String>,
     pub jira_url: Option<String>,
     pub user: String,
-    pub editor: Option<String>
+    pub editor: Option<String>,
 }
 
 pub struct WorklogMessage(pub String);
@@ -34,38 +34,35 @@ impl Configuration {
     }
 
     pub fn get_editor_command(&self) -> String {
-        env::var("EDITOR")
-            .unwrap_or(self.editor.clone().unwrap_or("nano".to_string()))
+        env::var("EDITOR").unwrap_or(self.editor.clone().unwrap_or("nano".to_string()))
     }
 }
 
 static NANO_ID_ALPHABET: [char; 16] = [
-        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f'
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f',
 ];
 pub fn get_nano_id() -> String {
-   nanoid!(10, &NANO_ID_ALPHABET)
+    nanoid!(10, &NANO_ID_ALPHABET)
 }
 
 pub fn get_started_date(started_date: &str) -> Result<DateTime<FixedOffset>, String> {
-    let date_time = 
-        NaiveDateTime::parse_from_str(started_date, "%Y-%m-%dT%H:%M")
-            .or_else(|_| date_time_from_time(started_date));
+    let date_time = NaiveDateTime::parse_from_str(started_date, "%Y-%m-%dT%H:%M")
+        .or_else(|_| date_time_from_time(started_date));
 
     match date_time {
-        Ok(value) => {
-            Local::now()
-                .offset()
-                .from_local_datetime(&value)
-                .single()
-                .ok_or_else(|| "Ambiguous or invalid datetime".to_string())
-        }
-        Err(_) => Err("Invalid started date, use 'YYYY-MM-DDTHH:MM' or 'H:M'.".to_string())
+        Ok(value) => Local::now()
+            .offset()
+            .from_local_datetime(&value)
+            .single()
+            .ok_or_else(|| "Ambiguous or invalid datetime".to_string()),
+        Err(_) => Err("Invalid started date, use 'YYYY-MM-DDTHH:MM' or 'H:M'.".to_string()),
     }
 }
 
 fn date_time_from_time(started_date: &str) -> Result<NaiveDateTime, ParseError> {
     let naive_time = NaiveTime::parse_from_str(started_date, "%H:%M")?;
     let today = Local::now().naive_local().date();
-    
+
     Ok(NaiveDateTime::new(today, naive_time))
 }
+
